88a090d9aec51405a97105cbab7f383f55a8e813
Luis Alfredo Villamizar Lopez <136473748+gorillaz68@users.noreply.github.com>
2023-06-13 18:36:42 -0400

    Create salida.h
    Módulo de función que es responsable: Límite o Borde
    Escribe todas las escrituras por pantalla del Usuario

---
+++ salida.h
@@ -0,0 +1,15 @@
+void escribir_Tam(int tam){
+   printf("escriba la cantidad de numeros a operar:%d\n",tam);
+}
+void escribir_num(int i){
+    printf("el numero %d es:%d\n", i+1,numeros[i]);
+}
+void escribir(int res){
+    printf("el resultado es %d\n",res);
+}//escribe el nuevo resultado hoy
+void escribir_Exit(){
+    printf("Hasta la Vista\n");
+}
+void escribir_exc(){
+    printf("Ingrese una opción válida\n");
+}
